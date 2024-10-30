262fb9905186e8e31020bd6d992273cc70cd6436
Graniska <nataly.granisk@gmail.com>
2024-10-30 08:52:17 -0300

    db.js updated

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,16 @@
 
-    const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-        
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER_ADDRESS}/${process.env.DB_NAME}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+};
+
+module.exports = connectDB;
